19fe3263721056d20c013c0f50a189d2835034a5
MISTER-Unchained <djangofakkeldij@gmail.com>
2024-05-15 16:23:00 +0200

    No damp

--- sim.py
+++ sim.py
@@ -5,22 +5,28 @@ from scipy.integrate import odeint, trapz
 from config import *
 from util import *
 
-t = np.linspace(0, 0.5, 20_000)
+
+def calc_polytrop_const(p, V):
+    return p * (V / m_piston) ** n
+
+
+p0 = vraag4_resultaat = bar_to_kpa(11.208200882187015)  # kPa
+V0 = h0 * A
+polytrop_c = calc_polytrop_const(p0, V0)
+state0 = (0, h0)
 
 
 def F_tot(p, v):
     F_g = m_piston * g  # N
-    F_f = c_w * v  # N
+    # F_f = c_w * v  # N
     F_pressure_out = bar_to_kpa(P_env) * A  # N
     F_pressure_in = p * A  # N
 
-    return -F_g - F_f - F_pressure_out + F_pressure_in  # N
+    return -F_g - F_pressure_out + F_pressure_in  # N
 
 
-def calc_p(h, T):
-    V = A * h  # m^3
-
-    return (R * T) / V  # kPa
+def calc_p(V):
+    return polytrop_c / (V / m_piston) ** n  # kPa
 
 
 @np.vectorize
@@ -31,18 +37,16 @@ def calc_dT(t):
 
 
 def functieVoorDeZuiger(state, t):
-    T, v, h = state
+    v, h = state
+    V = A * h  # m^3
+    p = calc_p(V)
+    a = F_tot(p, v)
+    return a, v
 
-    a = F_tot(calc_p(h, T), v)
-    dT = calc_dT(t)
 
-    return dT, a, v
-
-
-vraag_6_resultaat = 1329.6463680447187 + celcius_to_K  # K
-state0 = (vraag_6_resultaat, 0, h0)
+t = np.linspace(0, 0.5, 20_000)
 resultaat = odeint(functieVoorDeZuiger, state0, t)
-res_T, res_v, res_h = resultaat.T
+res_v, res_h = resultaat.T
 
 fig, ax = plt.subplots(1, 1)
 
